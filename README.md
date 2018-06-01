532a6f057ac2a1f0f0c224f0c748eec6eab6ad6c
JoMedeiros <josivan.medeiros@yahoo.com.br>
2018-06-01 11:56:38 -0300

    Some small modifications

--- README.md
+++ README.md
@@ -2,17 +2,27 @@
 
 ## Intro
 
-Essa é uma versão do jogo snake escrito em Assembly para processador MIPS.
+This is a version of classic snake game for MIPS processor written in Assembly.
 
-## Como usar
+## How to use
 
-Esse programa foi feito para ser excutado no
-simulador de MIPS Mars que pode ser baixado
-gratuitamente [aqui](http://courses.missouristate.edu/KenVollmar/mars/).
+To run the `.asm` file you can download a MIPS emulator. The MIPS Mars is
+a free emulator made in java that you can download [here](http://courses.missouristate.edu/KenVollmar/mars/).
+
+### The road so far...
+
+Currently the snake is moving, the program stops when it collides with the wall and that's it. The file `Snake.asm` has the most recent functional version of the "game". 
 
 ## TODO list
 
-- [x] Simular uma estrutura de fila (FIFO)
-- [ ] armazenar as posições dos pixels da cobra.
-- [x] Gerar a "maçã" aleatoriamente usando syscall 
-- [ ] Verificar se o ponto gerado coincide com algum ponto da cobra.
+- [x] Simulate a queue structure (FIFO).
+- [ ] Store the snake pixels positions int the queue.
+- [x] Generate random "apple" using syscall.
+- [x] Walls collision.
+- [ ] Verify if the "apple" collides with the snake.
+
+## Authors
+
+- [Josivan](https://www.github.com/JoMedeiros)
+- [Natalia ](https://github.com/bnatalha)
+
--- tests/Snake.asm
+++ tests/Snake.asm
@@ -1,5 +1,5 @@
 	.data
-queueStart:	.word	0x10011000
+queueBegin:	.word	0x10011000
 queueEnd:	.word	0x10012000
 screen:		.word 	0x10010000
 green:		.word	0x0000FF00
@@ -10,71 +10,75 @@ green:		.word	0x0000FF00
 main:
 
 	lw $t0, green
-	sw $t0, 0x10010084
-	sw $t0, 0x10010088
+#	sw $t0, 0x10010084
+#	sw $t0, 0x10010088
 
-	lw $s0, queueStart
+	lw $s0, queueBegin
 	lw $s1, queueEnd
-	li $s2, 0x10011000	# Início lógico da fila
-	li $s3, 0x10011004	# Fim lógico da fila
+	li $s2, 0x10011000	# Logic Begin of the Queue
+	li $s3, 0x10011004	# Logic End of the Queue
 	li $s4, 3		# x_pos
 	li $s5, 1		# y_pos
 	li $t1, 0x10010084
-	sw $t1, ($s2)	# Armazena a posição da tail na tela
+	sw $t1, ($s2)		# Stores the tail's position on the screen
 	
 	li $t1, 0x10010088
 	sw $t1, ($s3)
 	
 drawHead:
-	# Computing adress of (x, y)
+	# Computing Address of (x, y) on the screen
 	sll $t1, $s5, 5
 	add $t1, $t1, $s4
 	sll $t1, $t1, 2
-	li $t2, 0x10010000	# Primeiro endereço da tela
+	li $t2, 0x10010000	# Start address on the screen
 	add $t1, $t1, $t2
 	sw $t0, ($t1)
 	bne $s3, $s1, drawHead_if
 	add $s3, $s0, $zero	# <- else reset
 	j drawHead_fi
 drawHead_if:	
-	addi $s3, $s3, 4	# Aritmética de ponteiro
+	addi $s3, $s3, 4	# Pointer arithmetic
 drawHead_fi:
-	sw $t1, ($s3)		# Armazena o novo endereço da cabeça na fila
+	sw $t1, ($s3)		# Stores the new head address in the queue
 	
 eraseTail:
-	lw $t1, ($s2)		# Derrefenciando $s2 (eq en C à *pointer)
-	sw $zero, ($t1)		# Apaga o inicio da fila
+	lw $t1, ($s2)		# deferencing $s2 (eq in C to *pointer)
+	sw $zero, ($t1)		# Clear the tail on the screen
 	
 	bne $s2, $s1, eraseTail_if
 	add $s2, $s0, $zero	# <- else reset
 	j wait
 eraseTail_if:
-	addi $s2, $s2, 4	# Aritmética de ponteiro
+	addi $s2, $s2, 4	# Pointer arithmetic
 			
 wait:
 	li $a0, 500		#
-	li $v0, 32		# pause for 500 milisec
+	li $v0, 32		# Pause for 500 milisec
 	syscall			#
 	
-	lw $s1, 0xFFFF0004		# Verifica qual tecla foi pressionada
-					# OBS: esse valor fica no registrador até outra tecla ser pressionada
-	beq $s1, 0x00000077, moveUp 	# w está pressionado
-	beq $s1, 0x00000073, moveDown	# s está pressionado
-	beq $s1, 0x00000061, moveLeft	# a está pressionado
-	beq $s1, 0x00000064, moveRight	# d está pressionado
-	beq $s1, 0x00000071, quit	# q está pressionado
+	lw $s1, 0xFFFF0004		# Verifying which key is pressed
+					# OBS: This value stays on the register until another key is pressed
+	beq $s1, 0x00000077, moveUp 	# w key is pressed
+	beq $s1, 0x00000073, moveDown	# s key is pressed
+	beq $s1, 0x00000061, moveLeft	# a key is pressed
+	beq $s1, 0x00000064, moveRight	# d key is pressed
+	beq $s1, 0x00000071, quit	# q key is pressed
 	j moveRight
 moveUp:
 	subi, $s5, $s5, 1
+	beq $s5, -1, quit
 	j drawHead
 moveDown:
 	addi, $s5, $s5, 1
+	beq $s5, 32, quit
 	j drawHead
 moveLeft:
 	subi, $s4, $s4, 1
+	beq $s4, -1, quit
 	j drawHead
 moveRight:
 	addi, $s4, $s4, 1
+	beq $s4, 32, quit
 	j drawHead
 
 quit:
